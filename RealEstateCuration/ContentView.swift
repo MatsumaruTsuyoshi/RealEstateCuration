8ae63cd2dbfb88c2ed18975a913e32dcebe3fc41
MatsumaruTsuyoshi <strivesdfpx379@gmail.com>
2024-02-21 17:23:45 +0900

    タイトル変更

---
+++ RealEstateCuration/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RealEstateCuration
+//
+//  Created by matsumarutsuyoshi on 2024/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var path = NavigationPath()
+    
+    var body: some View {
+        NavigationStack(path: $path){
+            Button("遷移する"){
+            }
+            .navigationTitle("サラフド")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RealEstateCuration/RealEstateCurationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealEstateCurationApp.swift
+//  RealEstateCuration
+//
+//  Created by matsumarutsuyoshi on 2024/02/21.
+//
+
+import SwiftUI
+
+@main
+struct RealEstateCurationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
